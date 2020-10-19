9bd184a6bd9f1860ff2e2c6b58c195f1b1bbefc5
Nikolay-962 <nikpronin62@gmail.com>
2020-10-19 15:14:07 +0300

    игра

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const sliderItems = document.querySelectorAll('.slider__item');
+const sliderContent = document.querySelector('.slider__content');
+
+let active = 0;
+document.querySelector('.btn--next').onclick = () => {
+  sliderItems[active].classList.remove('active');
+  if (active + 1 == sliderItems.length) {
+    active = 0;
+  } else {
+    active++;
+  }
+  sliderItems[active].classList.add('active');
+}
+document.querySelector('.btn--prev').onclick = () => {
+  sliderItems[active].classList.remove('active');
+  if (active + 1 == sliderItems.length) {
+    active = 0;
+  } else {
+    active--;
+  }
+  sliderItems[active].classList.add('active');
+}
+
+window.onload = function () {
+  let sliderTexts = document.querySelectorAll('.slider__description');
+  for (let sliderText of sliderTexts) {
+    sliderText.onclick = activeItem;
+  }
+  function activeItem() {
+    this.classList.add('ansver');
+  }
+}
+
+
+
